5b7079a340ac5490b5e06e1e3ef5597f6afafe86
Kartikeya <kartikeya.anjul@gmail.com>
2025-06-20 12:37:43 +0530

    chain.json public file

--- Ballot_Block/src/server/BC server/blockchain.js
+++ Ballot_Block/src/server/BC server/blockchain.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const Block = require('./block');
+
+const CHAIN_FILE = path.join(__dirname, 'chain.json');
 
 class Blockchain {
   constructor() {
-    this.chain = [this.createGenesisBlock()];
+    this.chain = this.loadChain(); 
   }
 
   createGenesisBlock() {
@@ -15,16 +19,41 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.mineBlock('00'); // two leading zero for difficulty
+    newBlock.mineBlock('00'); 
     this.chain.push(newBlock);
+    this.saveChain();
   }
 
   hasUserVoted(userHash) {
-  return this.chain.some(block => {
-    const data = block.data;
-    return data.userIdHash === userHash;
-  });
-}
+    return this.chain.some(block => {
+      const data = block.data;
+      return data.userIdHash === userHash;
+    });
+  }
+
+  saveChain() {
+    fs.writeFileSync(CHAIN_FILE, JSON.stringify(this.chain, null, 2), 'utf-8');
+  }
+
+  loadChain() {
+    if (fs.existsSync(CHAIN_FILE)) {
+      try {
+        const raw = fs.readFileSync(CHAIN_FILE, 'utf-8');
+        const parsed = JSON.parse(raw);
+        return parsed.map(b => {
+          const block = new Block(b.index, b.timestamp, b.data, b.previousHash);
+          block.nonce = b.nonce;
+          block.hash = b.hash;
+          return block;
+        });
+      } catch (err) {
+        console.error('Error reading chain.json:', err);
+        return [this.createGenesisBlock()];
+      }
+    } else {
+      return [this.createGenesisBlock()];
+    }
+  }
 }
 
 module.exports = Blockchain;
